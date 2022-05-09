9eec4e8d07e76382ce7fe8c3e251bed08b7f7d14
PhucLH <hphuc23499@gmail.com>
2022-05-09 07:58:14 +0700

    fix private router

--- src/custom/PriveRouter.js
+++ src/custom/PriveRouter.js
@@ -3,10 +3,6 @@ import { Route, Redirect } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export const PriveRouter = (props) => {
-  const { currentUser, waiting } = useSelector((state) => state.auth);
-  if (waiting) {
-    toast.warning();
-  } else {
-    return currentUser ? <Route {...props} /> : <Redirect to="/" />;
-  }
+  const { currentUser } = useSelector((state) => state.auth);
+  return currentUser ? <Route {...props} /> : <Redirect to="/" />;
 };
